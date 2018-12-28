16d69bfa96647008a6c1930953aca5abbd7bb036
liyasthomas <liyascthomas@gmail.com>
2018-12-28 08:36:42 +0530

    :sparkles: Replaced 'Go home' button with 'Go back' button

--- src/my-404.js
+++ src/my-404.js
@@ -15,13 +15,17 @@ class My404 extends PolymerElement {
 					justify-content: center;
 					padding: 32px;
 					height: calc(100vh - 128px);
+					color: #b2b2b2;
 				}
 				h1 {
 					font-size: 3em;
+					line-height: 1;
+					margin: 0;
 				}
       </style>
-			<h1>4😕4</h1>
-			<div><a href="[[rootPath]]"><paper-fab icon="my-icons:arrow-back" aria-label="Back"></paper-fab></a></div>
+			<h1>404</h1>
+			<p>page not found</p>
+			<div style="margin-top: 32px;"><paper-fab icon="my-icons:arrow-back" aria-label="Back" onclick="history.back()"></paper-fab></div>
     `;
 	}
 }
